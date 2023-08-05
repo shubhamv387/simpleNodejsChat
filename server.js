4b77b8118f3809abae9d82dc0459620f9a441f4c
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-05 20:16:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    return fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      // console.log("printing " + data);
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        "<body><form action='/message' method='POST'> <input type='text' name='message'> </input><button type='submit'> Send</button> </form> </body>"
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunck) => {
+      body.push(chunck);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from Node Js Server!</h1> </body>");
+    res.write("</html>");
+    return res.end();
+  }
+});
+
+server.listen(3000);
